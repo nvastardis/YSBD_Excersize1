39c398774599bb34d726d1f6fbf24e4479a85bfb
Nick Vastardis <nick.vastar.2000@gmail.com>
2023-01-03 02:51:15 +0200

    Almost Complete HP_File.c

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 hp:
 	@echo " Compile hp_main ...";
-	gcc -I ./include/ -L ./lib/ -Wl,-rpath,./lib/ ./examples/hp_main.c ./src/record.c ./src/hp_file.c -lbf -o ./build/hp_main -O2
+	gcc -I ./include/ -L ./lib/ -Wl,-rpath,./lib/ ./examples/hp_main.c ./src/record.c ./src/hp_file.c -lbf -o ./build/hp_main -O2 -g3
 
 bf:
 	@echo " Compile bf_main ...";
@@ -9,4 +9,3 @@ bf:
 ht:
 	@echo " Compile hp_main ...";
 	gcc -I ./include/ -L ./lib/ -Wl,-rpath,./lib/ ./examples/ht_main.c ./src/record.c ./src/ht_table.c -lbf -o ./build/ht_main -O2
-
--- src/hp_file.c
+++ src/hp_file.c
@@ -15,104 +15,200 @@
   }                         \
 }
 
+#define RECORDS_PER_BLOCK BF_BLOCK_SIZE / sizeof(Record)
+
 typedef struct {
   int RecordCount;
   BF_Block* NextBlock;
 } HP_block_info;
 
+int SetUpNewBlock(HP_info *hp_info, Record record, BF_Block *previousBlock);
+
 
 int HP_CreateFile(char *fileName){
-  int fileDesc;
-  void *data;
-  HP_block_info *hpBlock;
-  BF_Block *firstBlock;
+    int fileDesc;
+    void *data;
+    HP_info *hp_info;
+    BF_Block *firstBlock;
 
-  CALL_BF(BF_CreateFile(fileName));
-  CALL_BF(BF_OpenFile(fileName, &fileDesc));
-  CALL_BF(BF_GetBlock(fileDesc, 0, firstBlock));
-  data = BF_Block_GetData(firstBlock);
-  Record* rec = data;
-  hpBlock = malloc(sizeof(HP_block_info));
-  hpBlock->RecordCount = 0;
-  hpBlock->NextBlock = NULL;
-  memcpy(&firstBlock + 10 + sizeof(HP_block_info), hpBlock, sizeof(HP_block_info));
-  CALL_BF(BF_UnpinBlock(firstBlock));
-  return 0;
+    if(BF_CreateFile(fileName)){
+        return -1;
+    }
+    if(BF_OpenFile(fileName, &fileDesc)){
+        return -1;
+    }
+    BF_Block_Init(&firstBlock);
+    if(BF_AllocateBlock( fileDesc, firstBlock)){
+        return -1;;
+    }
+
+    hp_info = (HP_info *) BF_Block_GetData(firstBlock);
+    hp_info->fileDescription = fileDesc;
+
+    BF_Block_SetDirty(firstBlock);
+    if(BF_UnpinBlock(firstBlock)){
+        return -1;
+    }
+    if(BF_CloseFile(fileDesc)){
+        return -1;
+    }
+    return 0;
 }
 
 HP_info* HP_OpenFile(char *fileName){
-  HP_info *fileInfo;
-  
-  int fileId;
-
-  CALL_BF(BF_OpenFile(fileName, &fileId));
-  fileInfo->fileDescription = fileId;
-  return fileInfo;
+    HP_info *fileInfo;
+    fileInfo = malloc(sizeof(HP_info*));
+    if(fileInfo == NULL){
+        return NULL;
+    }
+    if(BF_OpenFile(fileName, &fileInfo->fileDescription)){
+        return NULL;
+    }
+    return fileInfo;
 }
 
 
 int HP_CloseFile( HP_info *header_info ){
-  CALL_BF(BF_CloseFile(header_info->fileDescription));
-  free(header_info);
-  return 0;
+    int counter,
+        numberOfBlocks;
+
+    if(BF_GetBlockCounter(header_info->fileDescription, &numberOfBlocks)){
+        return -1;
+    }
+    for(counter = 0; counter < numberOfBlocks; counter++){
+        BF_Block *block;
+
+        BF_Block_Init(&block);
+        if(BF_GetBlock(header_info->fileDescription, counter, block)){
+            return -1;
+        }
+        BF_Block_Destroy(&block);
+    }
+
+    if(BF_CloseFile(header_info->fileDescription)){
+        return -1;
+    }
+    free(header_info);
+    return 0;
 }
 
 int HP_InsertEntry(HP_info *header_info, Record record){
-  int counter,
-      numberOfBlocks,
-      maxNumberOfRecordsPerBlock;
-
-  maxNumberOfRecordsPerBlock = BF_BLOCK_SIZE / sizeof(Record);
-  CALL_BF(BF_GetBlockCounter(header_info->fileDescription, &numberOfBlocks));
-
-  for(counter = 0; counter < numberOfBlocks; counter++){
+    int counter,
+        numberOfBlocks;
     void *data;
     BF_Block *block;
     Record *records;
     HP_block_info block_info;
-
-    CALL_BF(BF_GetBlock(header_info->fileDescription, counter, block));
-    data = BF_Block_GetData(block);
-    records = (Record*)data;
-    memcpy(&block_info, data+10, sizeof(HP_block_info));
-    if(block_info.RecordCount == maxNumberOfRecordsPerBlock){
-      continue;
+    if(BF_GetBlockCounter(header_info->fileDescription, &numberOfBlocks)){
+        return -1;
     }
-    records[block_info.RecordCount + 1] = record;
-    block_info.RecordCount ++;
-    BF_Block_SetDirty(block);
-    CALL_BF(BF_UnpinBlock(block));
+    
+    if(numberOfBlocks == 1){
+        return (SetUpNewBlock(header_info, record, NULL));
+    }
+    else{
+        BF_Block_Init(&block);
+        if(BF_GetBlock(header_info->fileDescription, numberOfBlocks - 1, block)){
+            return -1;
+        }
+        data = BF_Block_GetData(block);
+        memcpy(&block_info, data+10, sizeof(HP_block_info));
+        if(block_info.RecordCount ==RECORDS_PER_BLOCK){
+            return (SetUpNewBlock(header_info, record, block));
+        }
+        records = (Record*)data;
+        records[block_info.RecordCount] = record;
+        block_info.RecordCount++;
+        memcpy(data+10, &block_info, sizeof(HP_block_info));
+        BF_Block_SetDirty(block);
+        if(BF_UnpinBlock(block)){
+            return -1;
+        }
+    }
     return 0;
-  }
 
 }
 
 int HP_GetAllEntries(HP_info *header_info, int id){
-    int counter, blocksInFile, blockSearched;
+    int recordCounter, counter, blocksInFile, recordsSearched, fileFound;
+    void *data;
+    Record *records;
 
-    CALL_BF(BF_GetBlockCounter(header_info->fileDescription, &blocksInFile))
-    for(counter = 0; counter < blocksInFile; counter++){
-        int recordCounter;
-        void *data;
+    recordsSearched = 0;
+    fileFound = 0;
+    if(BF_GetBlockCounter(header_info->fileDescription, &blocksInFile)){
+        return -1;
+    }
+
+    for(counter = 1; counter < blocksInFile; counter++){
         BF_Block *block;
-        Record *records;
         HP_block_info block_info;
 
-        CALL_BF(BF_GetBlock(header_info->fileDescription, counter, block));
+        BF_Block_Init(&block);
+        if(BF_GetBlock(header_info->fileDescription, counter, block)){
+            return -1;
+        }
         data = BF_Block_GetData(block);
         records = (Record*) data;
         memcpy(&block_info, data+10, sizeof(HP_block_info));
         for(recordCounter = 0; recordCounter < block_info.RecordCount; recordCounter++){
             if(records[recordCounter].id == id){
-                fprintf(stdout, "%d\t%s\t%s\t%s", 
+                printf( "%d\t%s\t%s\t%s\t%s\n", 
                                 records[recordCounter].id,
                                 records[recordCounter].record,
                                 records[recordCounter].name,
                                 records[recordCounter].surname,
                                 records[recordCounter].city
                                 );
+                if(BF_UnpinBlock(block)){
+                    return -1;
+                }
+                return recordsSearched;
+                
             }
+            recordsSearched++;
+        }
+        if(BF_UnpinBlock(block)){
+            return -1;
         }
     }
-    return blocksInFile;
+    return -1;
+}
+
+int SetUpNewBlock(HP_info *hp_info, Record record, BF_Block *previousBlock){
+    int counter,
+        numberOfBlocks;
+    void *data;
+    BF_Block *block;
+    Record *blockData;
+    HP_block_info block_info;
+
+    BF_Block_Init(&block);
+    if(BF_AllocateBlock(hp_info->fileDescription, block)){
+        return -1;
+    }
+    data = BF_Block_GetData(block);
+    blockData = (Record *) data;
+    blockData[0] = record;
+    
+    block_info.NextBlock = NULL;
+    block_info.RecordCount = 1;
+    memcpy(data+10, &block_info, sizeof(HP_block_info));
+    
+    BF_Block_SetDirty(block);
+    if(BF_UnpinBlock(block)){
+        return -1;
+    }
+
+    if(previousBlock != NULL){
+        data = BF_Block_GetData(previousBlock);
+        memcpy(&block_info, data + 10, sizeof(HP_block_info));
+
+        block_info.NextBlock = block;
+
+        memcpy(data + 10, block, sizeof(HP_block_info));
+    }
+    return -1;
+
+    
 }
